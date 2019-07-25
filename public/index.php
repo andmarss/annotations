5e85e231da80e27938bf46b6cd30413410d43968
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-07-26 00:00:04 +0300

    simple service container

--- public/index.php
+++ public/index.php
@@ -1,13 +1,40 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
- use App\Format\{JSON,XML,YAML};
+use App\Format\{JSON,XML,YAML};
+use \App\Controller\IndexController;
+use \App\Container;
+use \App\Serializer;
 
 $data = [
     'name' => 'John',
     'surname' => 'Doe'
 ];
 
-$seriaizer = new \App\Serializer(new YAML());
+$seriaizer = new Serializer(new XML());
 
-var_dump($seriaizer->serialize($data));
+$controller = new IndexController($seriaizer);
+
+$container = new Container();
+
+$container->addService('format.json', function () use ($container){
+    return new JSON();
+});
+
+$container->addService('format.xml', function () use ($container){
+    return new XML();
+});
+
+$container->addService('format', function () use ($container){
+    $container->getService('format.json');
+});
+
+$container->addService('serializer', function () use ($container){
+    return new Serializer($container->getService('format'));
+});
+
+$container->addService('controller.index', function () use ($container){
+    return new IndexController($container->getService('serializer'));
+});
+
+var_dump($container);
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,7 +4,22 @@
 namespace App\Controller;
 
 
+use App\Serializer;
+
 class IndexController
 {
+    protected $serializer;
 
+    public function __construct(Serializer $serializer)
+    {
+       $this->serializer = $serializer;
+    }
+
+    public function index()
+    {
+       return $this->serializer->serialize([
+           'Action' => 'Index',
+           'Time' => time()
+       ]);
+    }
 }
